9a71c8f96399d140be7e5948de657ea415122156
dan-varughese <dan.varughese@outlook.com>
2024-04-19 13:58:51 -0400

    changes

--- main.py
+++ main.py
@@ -1,45 +1,53 @@
-dictionary = {}
-
 def main():
     book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
     print(f"--- Begin report of {book_path} ---")
-    print(f"There were {number_of_words(book_path)} words found in the document.")
-    dictionaryA = number_of_letters(book_path)
-    print(sorts(dictionaryA))
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
     print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def number_of_words(path):
-    with open(path) as f:
-        file_contents = f.read()
-        split_file = file_contents.split()
-        counter = 0
-        for word in split_file:
-            counter +=1
-        return counter
-    
-def number_of_letters(path):
-    with open(path) as f:
-        file_contentss = f.read()
-        file_contents = file_contentss.lower()
-        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        for letter in alphabet:
-            dictionary[letter] = file_contents.count(letter)
-        return dictionary
-
-def sorts(dictionary):
-    new_list = []
-    for char,count in dictionary.items():
-        new_dict = {'char': char, 'num': count}
-        new_list.append(new_dict)
-    return new_list
-        
-
-       
 
 
 main()
